ae598b0c7a3956042ff1e1284ed5bb5f0b9f7a85
Skullmasher Heartless <skullm4sher@gmail.com>
2020-04-18 14:25:04 +0200

    :skull: First working drag & drop on GMOD game

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
+const greetingMessage = function () {
+  console.log('  #####   ')
+  console.log(' #######  ')
+  console.log('#  ###  # ' + ' Hello There')
+  console.log('#   #   # ')
+  console.log('######### ' + ' Come contribute to the code !')
+  console.log(' ### ###  ')
+  console.log('  #####   ' + ' github.com/heartless-gaming/coronalan-planner')
+  console.log('  # # #   ')
+}
+
 const dragElmnts = () => {
-  console.log('Hello There!')
+
+  const games = document.querySelector('.game')
+  const dropboxs = document.querySelectorAll('.dropbox')
+  console.log(games)
+  function dragover(event) {
+    event.preventDefault()
+  }
+
+  function dragcenter(event) {
+    event.preventDefault()
+  }
+
+  function drop() {
+    this.append(games)
+  }
+
+  dropboxs.forEach((dropbox) => {
+    dropbox.addEventListener('dragover', dragover)
+    dropbox.addEventListener('dragcenter', dragcenter)
+    dropbox.addEventListener('drop', drop)
+  })
+
 }
 
 window.addEventListener('DOMContentLoaded', (event) => {
-    dragElmnts()
+  greetingMessage()
+  dragElmnts()
 });
